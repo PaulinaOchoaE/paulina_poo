0b23e96651f420f76c0ad682d493f1c29a2faa5c
paulina <paulina.8a.espinoza@gmail.com>
2023-09-15 11:34:53 -0600

    Tarea 4 Ventas de helados

---
+++ src/Edu/Paulina/RETO4/ui/main.java
@@ -0,0 +1,11 @@
+package edu.Paulina.RETO4.ui;
+
+import edu.Paulina.RETO4.ui.CLI;
+
+public class main {
+
+    public static void main(String[] args) {
+        edu.Paulina.RETO4.ui.CLI.showMenu();
+
+    }
+}
