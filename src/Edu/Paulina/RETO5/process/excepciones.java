635d4fa48afd6b90b259f54964c334f526a0f9d8
paulina <paulina.8a.espinoza@gmail.com>
2023-09-29 21:02:45 -0600

    Tarea 6 Excepciones puestas profe no puedo enviarle el CLI ni main no me deja porque no se

--- src/Edu/Paulina/RETO5/process/excepciones.java
+++ src/Edu/Paulina/RETO5/process/excepciones.java
@@ -119,7 +119,9 @@ public class excepciones {
     public static void Dificil() {
         String[] palabraDificil = {"a", "f", "r", "i", "c", "a", "n", "i", "s", "m", "o", "s"};
         boolean[] letrasAdivinadas = new boolean[palabraDificil.length];
-        System.out.println("¡Bienvenido al juego de adivinar la palabra Nivel:Dificil (palabra: 12 letras)!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("¡Bienvenido al juego de adivinar la palabra Nivel: Dificil (palabra: 12 letras)!");
 
         while (true) {
             try {
@@ -161,9 +163,9 @@ public class excepciones {
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
-            scanner.close();
-
         }
+
+        scanner.close(); // Este cierre de scanner debe estar fuera del while
     }
 
 }
